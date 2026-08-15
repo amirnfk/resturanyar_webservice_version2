900737c9c5188ef0ffb340a174a333fb779b76c8
Amir Nourafkan <77292004+amirnfk@users.noreply.github.com>
2026-08-15 20:46:03 +0330

    05_05_24 price bug fixed

--- obj/Release/net8.0/resturanyar.AssemblyInfo.cs
+++ obj/Release/net8.0/resturanyar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("resturanyar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c15aa7e011f63c6bc95ba6392aed3e21adf66dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a312c31a4b54759e1c5d537006aa8265bced3b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("resturanyar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("resturanyar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
